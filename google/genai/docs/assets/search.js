534f0da5e202a1174d2aa4d3df35380b13b3c9fe
Kaituo Huang <kaituo@google.com>
2025-01-29 13:31:30 -0800

    docs: Initial TypeDoc setup for TypeScript SDK
    PiperOrigin-RevId: 721089967

---
+++ google/genai/docs/assets/search.js
@@ -0,0 +1,7 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+window.searchData = "eJyrVirKLy9WsoqO1VHKzEtJrVCyqlYqSy0qzszPU7JSMtIz1rNU0lFKy0zNSQEpU8pLzE0FCiTn5+am5pUAWSn5yaVgZixUWVhqckl+EdxMoGElqSmeELNBQgWZBak5mXmpIF5tLQCoQCsl";
