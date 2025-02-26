df1ee11b8f53560ab8bacd9bdddc05e12fdd5b21
Amir Hardon <amirha@google.com>
2025-02-26 12:01:19 -0800

    chore: Install the dist tarball directly in the packaging test
    PiperOrigin-RevId: 731404028

--- test/packaging/test_packaging.sh
+++ test/packaging/test_packaging.sh
@@ -5,14 +5,6 @@ set -ex
 npm run build-prod
 TARBALL="$(npm pack)"
 
-TMPDIR="$(mktemp -d)"
-
-tar -zxvf "${TARBALL}" -C "${TMPDIR}"
-
-PACKAGE_PATH="${TMPDIR}/package"
-
 cd samples
-npm install "file:${PACKAGE_PATH}" --install-links
-# Now the samples package is using the `npm pack` output as @google/genai
-
+npm install "../${TARBALL}"
 npm run build
