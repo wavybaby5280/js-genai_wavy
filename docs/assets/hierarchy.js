c03988db88a767d97e4486f1bdb77cf7bd38fcf0
Amir Hardon <amirha@google.com>
2025-03-03 12:15:12 -0800

    docs: regenerate docs with the root-only client
    PiperOrigin-RevId: 733015075

--- docs/assets/hierarchy.js
+++ docs/assets/hierarchy.js
@@ -4,4 +4,4 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-window.hierarchyData = "eJyrVirKzy8pVrKKjtVRKkpNy0lNLsnMzwMKVNfWAgCbHgqm"
+window.hierarchyData = "eJyrVirKzy8pVrKKjtVRKkpNy0lNLsnMzytWsqqurQUAmx4Kpg=="
