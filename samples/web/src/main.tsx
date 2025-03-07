4135dceddfea817094107efed7c978da6e475377
Amir Hardon <amirha@google.com>
2025-03-07 06:36:43 -0800

    docs: Use Bootstrap to improve the web sample's appearance
    PiperOrigin-RevId: 734536595

--- samples/web/src/main.tsx
+++ samples/web/src/main.tsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+// Import our custom CSS
+import './scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- samples/web/vite.config.ts
+++ samples/web/vite.config.ts
@@ -5,8 +5,14 @@
  */
 import react from '@vitejs/plugin-react';
 import {defineConfig} from 'vite';
+import {resolve} from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
 });
