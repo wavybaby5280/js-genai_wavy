12a6e71dce49e9b1a5b8e569bd325bdfb695bf51
Ivan Lopez Hernandez <ivanlh@google.com>
2025-03-27 15:17:37 -0700

    chore: Add Generate Video Sample
    PiperOrigin-RevId: 741298746

---
+++ sdk-samples/generate_video.ts
@@ -0,0 +1,72 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import {GoogleGenAI} from '@google/genai';
+
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+const GOOGLE_CLOUD_PROJECT = process.env.GOOGLE_CLOUD_PROJECT;
+const GOOGLE_CLOUD_LOCATION = process.env.GOOGLE_CLOUD_LOCATION;
+const GOOGLE_GENAI_USE_VERTEXAI = process.env.GOOGLE_GENAI_USE_VERTEXAI;
+
+async function delay(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function generateContentFromMLDev() {
+  const ai = new GoogleGenAI({vertexai: false, apiKey: GEMINI_API_KEY});
+  var operation = await ai.models.generateVideos({
+    model: 'veo-2.0-generate-001',
+    prompt: 'Man with a dog',
+    config: {
+      numberOfVideos: 1,
+    },
+  });
+
+  while (!operation.done) {
+    console.log('Waiting for completion');
+    await delay(1000);
+    operation = await ai.operations.get({operation: operation});
+  }
+
+  const response = operation.result;
+  const fileName = response?.generatedVideos?.[0].video?.uri;
+  console.log(fileName);
+}
+
+async function generateContentFromVertexAI() {
+  const ai = new GoogleGenAI({
+    vertexai: true,
+    project: GOOGLE_CLOUD_PROJECT,
+    location: GOOGLE_CLOUD_LOCATION,
+  });
+  var operation = await ai.models.generateVideos({
+    model: 'veo-2.0-generate-001',
+    prompt: 'Man with a dog',
+  });
+
+  while (!operation.done) {
+    console.log('Waiting for completion');
+    await delay(1000);
+    operation = await ai.operations.get({operation: operation});
+  }
+
+  const response = operation.result;
+  const fileName = response?.generatedVideos?.[0].video?.uri;
+  console.log(fileName);
+}
+
+async function main() {
+  if (GOOGLE_GENAI_USE_VERTEXAI) {
+    await generateContentFromVertexAI().catch((e) =>
+      console.error('got error', e),
+    );
+  } else {
+    await generateContentFromMLDev().catch((e) =>
+      console.error('got error', e),
+    );
+  }
+}
+
+main();
